333f85f2255c206b4e80377e1f19917de08da7fa
Sarika-Choudhary <choudharysarikafeb@gmail.com>
2026-08-04 11:23:40 +0530

    Time: 0 ms (100%), Space: 10.9 MB (74.26%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void postorder(TreeNode* root, vector<int>& result) {
+        if (!root) return;
+        
+        postorder(root->left, result);
+        postorder(root->right, result);
+        result.push_back(root->val);
+    }
+    
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        postorder(root, result);
+        return result;
+    }
+};
