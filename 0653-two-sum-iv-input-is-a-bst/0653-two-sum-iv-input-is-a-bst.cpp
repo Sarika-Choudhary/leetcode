f4cb972bd7852e5c12b4ee7cdb28e59d92889b7d
Sarika-Choudhary <choudharysarikafeb@gmail.com>
2026-08-09 13:20:50 +0530

    Time: 7 ms (27.37%), Space: 38.7 MB (33.61%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,40 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right)
- *         : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<int> inorderValues;
-    void inorder(TreeNode* root)
-    {
-        if (root==NULL)
-            return;
-        inorder(root->left);
-        inorderValues.push_back(root->val);
-        inorder(root->right);
-    }
-    bool findTarget(TreeNode* root, int k)
-    {
-        inorder(root);
-        int left =0;
-        int right= inorderValues.size() - 1;
-        while (left<right)
-        {
-            int sum =inorderValues[left] + inorderValues[right];
-            if (sum == k)
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> s;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* curr = q.front();
+            q.pop();
+            int need = k - curr->val;
+            if (s.find(need) != s.end())
                 return true;
-            if (sum < k)
-                left++;
-            else
-                right--;
+            s.insert(curr->val);
+            if (curr->left != NULL)
+                q.push(curr->left);
+            if (curr->right != NULL)
+                q.push(curr->right);
         }
         return false;
     }
