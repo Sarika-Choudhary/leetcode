fe072b3f33174174006c3214b9997f608fd173e0
Sarika-Choudhary <choudharysarikafeb@gmail.com>
2026-09-24 16:15:34 +0530

    Time: 81 ms (23.18%), Space: 111.4 MB (28.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
-            return true;
-            }
-        }
-        return false;
+      unordered_set<int> seen;
+      for(int i=0;i<nums.size();i++){
+        if(seen.find(nums[i])!=seen.end()){
+        return true;
+      }  seen.insert(nums[i]);
+      }
+      return false;
     }
 };
